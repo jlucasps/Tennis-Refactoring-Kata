d958cb63f644d3a5abb7cdcb5195dc7918489fe5
Bruno Leite Alves <bleite@thoughtworks.com>
2015-06-03 21:03:39 -0300

    more code extractions into functions

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -12,8 +12,6 @@ TennisGame1.prototype.wonPoint = function(playerName) {
         this.player2Score += 1;
 };
 
-
-
 TennisGame1.prototype.getScore = function() {
     var score = "";
     var tempScore = 0;
@@ -34,11 +32,31 @@ TennisGame1.prototype.getScore = function() {
     }
 
     var getScoreForAdvantageOrWon = function(player1Score, player2Score){
+        var score;
         var scoreDifference = player1Score - player2Score;
         if (scoreDifference === 1) score = "Advantage player1";
         else if (scoreDifference === -1) score = "Advantage player2";
         else if (scoreDifference >= 2) score = "Win for player1";
         else score = "Win for player2";
+        return score;
+    };
+
+    var getScore = function(tempScore) {
+        var scores = ["Love", "Fifteen", "Thirty", "Forty"];
+
+        return scores[tempScore];
+    };
+
+    var getScoreForAdvantage = function(player1Score, player2Score){
+        for (var i = 1; i < 3; i++) {
+            if (i === 1) tempScore = player1Score;
+            else {
+                score += "-";
+                tempScore = player2Score;
+            }
+
+            score += getScore(tempScore);
+        }
         return score;
     }
 
@@ -47,27 +65,7 @@ TennisGame1.prototype.getScore = function() {
     } else if (isAdvantageOrWonGame(this.player1Score, this.player2Score)) {
         score = getScoreForAdvantageOrWon(this.player1Score, this.player2Score);
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.player1Score;
-            else {
-                score += "-";
-                tempScore = this.player2Score;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        score = getScoreForAdvantage(this.player1Score, this.player2Score);
     }
     return score;
 };
